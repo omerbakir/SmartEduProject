616e64793602f71091b668440d0b73983233d449
omerfarukbakir <omerfarukbakir@gmail.com>
2023-05-10 16:54:00 +0300

    Routing

--- app.js
+++ app.js
@@ -1,25 +1,29 @@
 const express = require("express");
+const mongoose = require("mongoose")
+const pageRoute = require("./routes/pageRoute")
+const courseRoute = require("./routes/courseRoute")
 
 const app = express();
+
+//connect DB
+mongoose.connect("mongodb://localhost/smartedu-db").then(() => {
+    console.log("DB bağlantısı Yapıldı")
+})
 
 //Template Engine
 app.set("view engine", "ejs")
 
 //Middlewares
 app.use(express.static("public"))
+app.use(express.urlencoded({
+    extended: true
+}))
+app.use(express.json())
 
 //Routes
-app.get("/", (req, res) => {
-    res.status(200).render("index", {
-        page_name: "index"
-    })
-})
+app.use("/", pageRoute)
+app.use("/courses", courseRoute)
 
-app.get("/about", (req, res) => {
-    res.status(200).render("about", {
-        page_name: "about"
-    })
-})
 
 const port = 3000;
 app.listen(port, () => {
