320351e543f215298319630d8e3c32db6b4f8f1d
omerfarukbakir <omerfarukbakir@gmail.com>
2023-06-15 16:41:03 +0300

    Courses & Teacher

--- controllers/courseController.js
+++ controllers/courseController.js
@@ -3,7 +3,12 @@ const Category = require("../models/Category")
 
 exports.createCourse = async (req, res) => {
     try {
-        const course = await Course.create(req.body);
+        const course = await Course.create({
+            name: req.body.name,
+            description: req.body.name,
+            category: req.body.category,
+            user: req.session.userID
+        });
         res.status(201).redirect("/courses")
     } catch (error) {
         res.status(400).json({
@@ -46,7 +51,7 @@ exports.getCourse = async (req, res) => {
     try {
         const course = await Course.findOne({
             slug: req.params.slug
-        })
+        }).populate("user")
         res.status(200).render("course", {
             course,
             page_name: "courses"
