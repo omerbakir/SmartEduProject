fd81a495f4842896fe8de5e0b92f117ef1bc6fff
omerfarukbakir <omerfarukbakir@gmail.com>
2023-06-16 15:02:23 +0300

    Courses & Student

--- controllers/authController.js
+++ controllers/authController.js
@@ -30,12 +30,12 @@ exports.loginUser = async (req, res) => {
         if (user) {
             const same = await bcrypt.compare(password, user.password);
 
-            if (same) {
+            
                 // USER SESSION
                 req.session.userID= user._id
                 res.status(200).redirect("/users/dashboard");
                 return;
-            }
+            
         }
     } catch (error) {
         res.status(400).json({
@@ -52,7 +52,7 @@ exports.logoutUser = (req,res)=>{
 }
 
 exports.getDashboardPage = async (req, res) => {
-    const user = await User.findOne({_id:req.session.userID})
+    const user = await User.findOne({_id:req.session.userID}).populate("courses")
     const categories= await Category.find()
     const courses= await Course.find({user:req.session.userID})
     res.status(200).render("dashboard", {
